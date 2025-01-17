67214767770fcc750f8f2ae830881c66e8e1466d
Anurag Ojha <aojharaj2004@gmail.com>
2025-01-17 16:37:06 +0530

    Your commit message

---
+++ main.js
@@ -0,0 +1,111 @@
+class Game{
+    constructor(canvas,context){
+        this.canvas=canvas;
+        this.ctx=context;
+        this.width;
+        this.height;
+        
+        this.cellSize=50;
+        this.columns;
+        this.rows;
+        this.eventTimer=0;
+        this.eventInterval=200;
+        this.eventUpdate=false;
+
+        // this.resize(window.innerWidth, window.innerHeight);
+
+        this.player1;
+        this.player2;
+        this.player3;
+        this.player4;
+        this.food;
+        this.gameObjects;
+
+        window.addEventListener('resize',e=>{
+            this.resize(e.currentTarget.innerWidth,e.currentTarget.innerHeight);
+            
+        });
+        this.resize(window.innerWidth,window.innerHeight)
+    }
+    resize(width,height){
+        this.canvas.width=width-width%this.cellSize;
+        this.canvas.height=height-height%this.cellSize;
+        this.ctx.fillStyle='blue';
+        this.ctx.font='30px Impact';
+        this.ctx.textBaseline='top';
+        this.width=this.canvas.width;
+        this.height=this.canvas.height;
+        this.columns=Math.floor(this.width/this.cellSize);
+        this.rows=Math.floor(this.height/this.cellSize);
+
+        this.player1=new Keyboard1(this,0,0,1,0,'magenta');
+        this.player2=new ComputerAi(this,this.columns-1,0,0,1,'orangered');
+        this.player3=new ComputerAi(this,this.columns-1,this.rows-1,-1,0,'yellow');
+        this.player4=new ComputerAi(this,0,this.rows-1,0,-1,'darkblue');
+        this.food=new Food(this);
+        this.gameObjects=[this.player1,this.player2,this.player3,this.player4,this.food];
+    }
+    drawGrid(){
+        for(let y=0;y<this.rows;y++)
+        {
+            for(let x=0;x<this.columns;x++){
+                this.ctx.strokeRect(x*this.cellSize,y*this.cellSize,this.cellSize,this.cellSize)
+            }
+        }
+        
+    }
+    drawStatusText(){
+        this.ctx.fillText('P1: '+this.player1.score,this.cellSize,this.cellSize);
+        this.ctx.fillText('P2: '+this.player2.score,this.cellSize,this.cellSize*2);
+        this.ctx.fillText('P3: '+this.player3.score,this.cellSize,this.cellSize*3);
+        this.ctx.fillText('P1: '+this.player4.score,this.cellSize,this.cellSize*4);
+    }
+    checkCollision(a,b){
+        return a.x===b.x&&a.y===b.y;
+    }
+    handlePeriodicEvents(deltaTime){
+        if(this.eventTimer<this.eventInterval)
+        {
+            this.eventTimer+=deltaTime;
+            this.eventUpdate=false;
+        }
+        else
+        {
+            this.eventTimer=0;
+            this.eventUpdate=true;
+        }
+    }
+    render(deltaTime){ 
+        this.handlePeriodicEvents(deltaTime);
+        if(this.eventUpdate)
+        {
+            this.ctx.clearRect(0,0,this.width,this.height);
+            this.drawGrid();
+        //this.ctx.fillStyle='blue';
+            this.gameObjects.forEach(object=>{
+                object.draw();
+                object.update();
+            })
+            this.drawStatusText();
+        }
+    }
+}
+
+window.addEventListener('load',function(){
+    const canvas=this.document.getElementById('canvas1');
+    const ctx=canvas.getContext('2d');
+    canvas.width=window.innerWidth;
+    canvas.height=window.innerHeight;
+    
+
+    const game=new Game(canvas,ctx);
+    let lastTime=0;
+    function animate(timeStamp){
+        const deltaTime=timeStamp-lastTime;
+        lastTime=timeStamp; 
+        game.render(deltaTime);
+        requestAnimationFrame(animate);
+    }
+    requestAnimationFrame(animate);
+    
+})
---
+++ snake.js
@@ -0,0 +1,118 @@
+class Snake{//parent class super
+    constructor(game,x,y,speedX,speedY,color){
+        this.game=game;
+        this.x=x;
+        this.y=y;
+        this.speedX=speedX;
+        this.speedY=speedY;
+        this.color=color;
+        this.width=this.game.cellSize;
+        this.height=this.game.cellSize;
+        this.moving=true;
+        this.score=0;
+    }
+    update(){
+        //check collision
+        if(this.game.checkCollision(this,this.game.food)){
+            this.game.food.reset();
+            this.score++;
+        }
+        //boundries
+        if(this.x<=0&&this.speedX<0||
+            this.x>=this.game.columns-1&&this.speedX>0||
+            this.y<=0&&this.speedY<0||
+            this.y>=this.game.rows-1&&this.speedY>0
+        )
+        {
+            this.moving=false;
+        }
+        if(this.moving){
+            this.x+=this.speedX;
+            this.y+=this.speedY;
+        }
+    }
+    draw(){
+        this.game.ctx.fillStyle=this.color;
+        this.game.ctx.fillRect(this.x*this.game.cellSize,this.y*this.game.cellSize,this.width,this.height)
+    }
+    turnUp(){
+        this.speedX=0;
+        this.speedY=-1;
+        this.moving=true;
+
+    }
+    turnDown(){
+        this.speedX=0;
+        this.speedY=1;
+        this.moving=true;
+        
+    }
+    turnLeft(){
+        this.speedX=-1;
+        this.speedY=0;
+        this.moving=true;
+        
+    }
+    turnRight(){
+        this.speedX=1;
+        this.speedY=0;
+        this.moving=true;
+        
+    }
+}
+
+class Keyboard1 extends Snake{//child class sub
+    constructor(game,x,y,speedX,speedY,color){
+        super(game,x,y,speedX,speedY,color);
+
+        window.addEventListener('keydown',e=>{
+            
+            if(e.key==='ArrowUp') this.turnUp();
+            else if(e.key==='ArrowDown') this.turnDown();
+            else if(e.key==='ArrowLeft') this.turnLeft();
+            else if(e.key==='ArrowRight') this.turnRight();   
+        })
+    }
+}
+
+class Keyboard2 extends Snake{//child class sub
+    constructor(game,x,y,speedX,speedY,color){
+        super(game,x,y,speedX,speedY,color);
+
+        window.addEventListener('keydown',e=>{
+            
+            if(e.key.toLowerCase()==='w') this.turnUp();
+            else if(e.key.toLowerCase()==='s') this.turnDown();
+            else if(e.key.toLowerCase()==='a') this.turnLeft();
+            else if(e.key.toLowerCase()==='d') this.turnRight();   
+        })
+    }
+}
+
+class ComputerAi extends Snake{
+    constructor(game,x,y,speedX,speedY,color){
+        super(game,x,y,speedX,speedY,color);
+        this.turnTimer=0;
+        this.turnInterval=Math.floor(Math.random()*this.game.columns+1);
+
+    }
+
+    update(){
+        super.update();
+        if(this.turnTimer<this.turnInterval){
+            this.turnTimer+=1;
+        }
+        else{
+            this.turnTimer=0;
+            this.turn();
+            this.turnInterval=Math.floor(Math.random()*this.game.columns+1);
+        }
+    }
+    turn(){
+        if(this.speedY===0){
+            Math.random()<0.5?this.turnUp():this.turnDown();
+        } else if(this.speedX===0){
+            Math.random()<0.5?this.turnLeft():this.turnRight();
+        }
+    }
+}
